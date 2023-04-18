1c6bc2fa8601133d15b6bc6380179a68c689ff61
Katuri Bhanu Venkata Krishna <93046500+b-v-krishna@users.noreply.github.com>
2023-04-18 17:08:00 +0530

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -7,5 +7,5 @@ st.markdown("Currently,I'm pursuing final year of :green[B.TECH in CSE]")
 st.markdown("I'm interested in learning Data science,Intern at :red[INNOMATICS RESEARCH LABS] ")
 
 st.write("connect with me on :")
-st.write("[linkedin](https://www.linkedin.com/in/bhanu-katuri-45237b218/)")
+st.write("[linkedin](https://www.linkedin.com/in/bhanu-katuri/)")
 st.write("[Github](https://github.com/b-v-krishna)")
