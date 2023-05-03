c96b94a2ef50197e3de784321ad1d133f46bf5cb
Katuri Bhanu Venkata Krishna <93046500+b-v-krishna@users.noreply.github.com>
2023-05-03 17:56:36 +0530

    Update Locations_of_pub.py

--- Open_Pubs/pages/Locations_of_pub.py
+++ Open_Pubs/pages/Locations_of_pub.py
@@ -5,8 +5,12 @@ from streamlit_folium import folium_static
 
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path to this file's root directory
 PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+# absolute path of directory_of_interest
 dir_of_interest = os.path.join(PARENT_DIR, "resources")
+
+
 DATA_PATH = os.path.join(dir_of_interest, "data", "pub_df.csv")
 
 data=pd.read_csv(DATA_PATH)
