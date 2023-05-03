051c276a4198727d357fa64d8924a5489ad6e0a8
Katuri Bhanu Venkata Krishna <93046500+b-v-krishna@users.noreply.github.com>
2023-05-03 17:42:19 +0530

    Update home.py

--- Open_Pubs/home.py
+++ Open_Pubs/home.py
@@ -12,11 +12,9 @@ st.image(pic, width=700)
 st.title(':red[Welcome to Open Pub Application]')
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
-# absolute path to this file's root directory
 PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
-# absolute path of directory_of_interest
 dir_of_interest = os.path.join(PARENT_DIR, "resources")
-DATA_PATH = os.path.join(dir_of_interest, "data", "pub_df.csv")
+DATA_PATH = "/app/innomatics-intern-tasks/Open_Pubs/resources/data/pub_df.csv"
 
 pub_df=pd.read_csv(DATA_PATH)
 
